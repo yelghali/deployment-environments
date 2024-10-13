2259776128e4b28b9dedddde54e4a9f09adc7dee
Yassine El Ghali <Yassine.el.ghali@microsoft.com>
2024-10-13 22:40:28 +0200

    TF hello world

--- Environments/TerraformHelloWorld/main.tf
+++ Environments/TerraformHelloWorld/main.tf
@@ -1,6 +1,29 @@
-provider "azurerm" {
-  features {}
+# Configure desired versions of terraform, azurerm provider
+terraform {
+  required_version = ">= 1.1.7, < 2.0.0"
+  required_providers {
+    azurerm = {
+      version = "~>3.97.1"
+      source  = "hashicorp/azurerm"
+    }
+  }
 }
+
+# Enable features for azurerm
+provider "azurerm" {
+  skip_provider_registration = "true"
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+# Access client_id, tenant_id, subscription_id and object_id configuration values
+data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "example" {
   name     = "ade-generated-resources"
